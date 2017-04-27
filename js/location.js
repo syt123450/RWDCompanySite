1517b2bc1ccce358e6c937a1298c49c5ae27dea9
jinchengcheng <15671675579@163.com>
2017-04-26 17:55:07 -0700

    add management page and location page

--- js/location.js
+++ js/location.js
@@ -1,3 +1,18 @@
 /**
  * Created by ss on 2017/4/22.
  */
+
+
+function initMap() {
+var mapOptions = {
+    center: new google.maps.LatLng(37.33, -121.88),
+    zoom: 15,
+    mapTypeId: 'roadmap'
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    
+ var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(37.3352, -121.88),
+          map: map,
+          title: 'Welcome to San Jose State University!'})
+}
--- js/management.js
+++ js/management.js
@@ -1,3 +1,76 @@
 /**
  * Created by ss on 2017/4/22.
  */
+     $(function(){
+
+           $("#div1").hide(); 
+           $("#div2").hide();
+         $("#div3").hide();
+         $("#div4").hide();
+         $("#div5").hide();
+         $("#div6").hide();
+         
+            $("#span1").click(function(){
+
+                  $("#div1").fadeIn("slow");
+
+            });
+            $("#span2").click(function(){
+
+                  $("#div2").fadeIn("slow");
+
+            });
+            $("#span3").click(function(){
+
+                  $("#div3").fadeIn("slow");
+
+            });
+            $("#span4").click(function(){
+
+                  $("#div4").fadeIn("slow");
+
+            });
+            $("#span5").click(function(){
+
+                  $("#div5").fadeIn("slow");
+
+            });
+            $("#span6").click(function(){
+
+                  $("#div6").fadeIn("slow");
+
+            });
+         
+
+            $("#span7").click(function(){
+
+                     $("#div1").fadeOut("slow");
+
+            })
+          $("#span8").click(function(){
+
+                     $("#div2").fadeOut("slow");
+
+            })
+          $("#span9").click(function(){
+
+                     $("#div3").fadeOut("slow");
+
+            })
+          $("#span10").click(function(){
+
+                     $("#div4").fadeOut("slow");
+
+            })
+          $("#span11").click(function(){
+
+                     $("#div5").fadeOut("slow");
+
+            })
+          $("#span12").click(function(){
+
+                     $("#div6").fadeOut("slow");
+
+            })
+
+        });
