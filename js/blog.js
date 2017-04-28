8e9358132c524ac05f2e64486c84fc7fa7efa847
MiaoS <miaoshi2013@gmail.com>
2017-04-28 16:06:55 -0700

    reformat

--- js/blog.js
+++ js/blog.js
@@ -1,16 +1,13 @@
 /**
  * Created by ss on 2017/4/22.
  */
-$(document).ready(function($){
+$(document).ready(function ($) {
 
+	$(".panel-body").on("click", function (e) {
+		var link = $(this).data("link");
 
-
-	$(".panel-body").on("click",function(e){
-		var link= $(this).data("link");
-
-		
 		$('[id^=blog_content-]').hide();
-		$('#blog_content-'+link).show();
+		$('#blog_content-' + link).show();
 	});
 
 });
