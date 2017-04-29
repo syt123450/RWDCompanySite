bee4091c81d5c81cb963ed66c23936b0ea30d0c7
anna199 <liranlr1992@gmail.com>
2017-04-29 16:50:56 -0700

    Add conduct.html and download.html

--- js/location.js
+++ js/location.js
@@ -12,14 +12,26 @@ $(function () {
 
 function initMap() {
 var mapOptions = {
-    center: new google.maps.LatLng(37.33, -121.88),
+    center: new google.maps.LatLng(37.335, -121.88),
     zoom: 15,
     mapTypeId: 'roadmap'
 }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    
- var marker = new google.maps.Marker({
+
+var marker = new google.maps.Marker({
           position: new google.maps.LatLng(37.3352, -121.88),
           map: map,
           title: 'Welcome to San Jose State University!'})
+
+var mapOptions2 = {
+              center: new google.maps.LatLng(37.548, -121.9886),
+              zoom: 15,
+              mapTypeId: 'roadmap'
+          }
+var map2 = new google.maps.Map(document.getElementById("map2"), mapOptions2);
+
+var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(37.548271, -121.988571),
+                    map: map2,
+                    title: 'Welcome to San Jose State University!'})
 }
