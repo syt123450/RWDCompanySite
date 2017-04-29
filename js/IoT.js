0ac851036234a3eaf2640d9387664534351e2f19
Qi Liu <liuqi0087@gmail.com>
2017-04-29 11:24:47 -0700

    Add mobile,bigData and loT pages

--- js/IoT.js
+++ js/IoT.js
@@ -2,10 +2,35 @@
  * Created by ss on 2017/4/22.
  */
 $(function () {
+  $("#downloadBrochure").hide();
+  $("#downloadWhitePaper").hide();
 
+  $("#BrochureButton").click(function () {
+      $("#downloadBrochure").fadeIn("slow");
+  });
+
+  $("#WhitePaperButton").click(function () {
+      $("#downloadWhitePaper").fadeIn("slow");
+  });
+
+  $("#span01").click(function () {
+    $("#downloadBrochure").hide();
+ });
+
+ $("#cancel1").click(function () {
+   $("#downloadBrochure").hide();
+});
+
+$("#span02").click(function () {
+  $("#downloadWhitePaper").hide();
+});
+
+$("#cancel2").click(function () {
+ $("#downloadWhitePaper").hide();
+});
     // connect();
     //
     // $(window).unload(function(){
     //     disconnect();
     // });
-})
+});
--- js/bigData.js
+++ js/bigData.js
@@ -2,7 +2,32 @@
  * Created by ss on 2017/4/22.
  */
 $(function () {
+  $("#downloadBrochure").hide();
+  $("#downloadWhitePaper").hide();
 
+  $("#BrochureButton").click(function () {
+      $("#downloadBrochure").fadeIn("slow");
+  });
+
+  $("#WhitePaperButton").click(function () {
+      $("#downloadWhitePaper").fadeIn("slow");
+  });
+
+  $("#span01").click(function () {
+    $("#downloadBrochure").hide();
+ });
+
+ $("#cancel1").click(function () {
+   $("#downloadBrochure").hide();
+});
+
+$("#span02").click(function () {
+  $("#downloadWhitePaper").hide();
+});
+
+$("#cancel2").click(function () {
+ $("#downloadWhitePaper").hide();
+});
     // connect();
     //
     // $(window).unload(function(){
